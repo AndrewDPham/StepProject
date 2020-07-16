0fa792f9d82605ed002f4b7d3c121a68414b5f9f
Your Name <you@example.com>
2020-07-16 16:36:24 +0000

    Build half of query function

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -1,3 +1,4 @@
+
 // Copyright 2019 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,9 +16,86 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+        // throw new UnsupportedOperationException("TODO: Implement this method.");
+
+        Collection<TimeRange> timeRanges = new ArrayList<TimeRange>();
+
+        List<TimeRange> attendeeRelevantTimeRanges = new ArrayList<TimeRange>();
+        Collection<String> requestAttendees = request.getAttendees(); 
+        Collection<String> eventAttendees;
+
+        for(Event event : events){
+	        eventAttendees = event.getAttendees(); 
+	        if(!Collections.disjoint(requestAttendees, eventAttendees)){
+	            attendeeRelevantTimeRanges.add(event.getWhen());
+            }
+        }
+        
+        Collections.sort(attendeeRelevantTimeRanges, TimeRange.ORDER_BY_START);
+
+        if(attendeeRelevantTimeRanges.size() == 0){
+            if(request.getDuration() > TimeRange.WHOLE_DAY.duration()){
+                return timeRanges;
+            }
+            timeRanges.add(TimeRange.WHOLE_DAY);
+            return timeRanges;
+        }
+
+        //Add from the beginning of the day until start of first meeting if duration is valid
+        TimeRange current = attendeeRelevantTimeRanges.get(0);
+        TimeRange toAdd = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, current.start(), false);
+        if(toAdd.duration() >= request.getDuration()){
+                timeRanges.add(toAdd);
+        }
+        
+        
+        
+
+        return timeRanges;
+    }
 }
+
+        
+
+	        // if( i == 0 ){
+		    //     timeRanges.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, attendeeRelevantTimeRanges.get(0).start(), false));
+	        // }
+ 
+	        // if( i == attendeeRelevantTimeRanges.size() - 1){
+		    //     timeRanges.add(TimeRange.fromStartEnd(attendeeRelevantTimeRanges.get(attendeeRelevantTimeRanges.size() - 1).end(), TimeRange.END_OF_DAY, true));
+            // }
+
+
+
+
+//   Event Schema:
+//   private final String title;
+//   private final TimeRange;
+//   private final Set<String> attendees = new HashSet<>();
+ 
+//   MeetingRequest Schema:
+//   private final Collection<String> attendees = new HashSet<>();
+//   private final Collection<String> optional_attendees = new HashSet<>();
+//   private final long duration;
+ 
+//   TimeRange Schema:
+//   private final int start;
+//   private final int duration;
+ 
+// Check for conflict;
+
+
+
+/*
+    Iterate through events
+    
+
+    Add time range
+*/
